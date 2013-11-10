fe8314ba6b7e4f843dfff06994aedac9565ec552
Nicklas Erman <nicklas.erman@gmail.com>
2013-11-10 13:43:23 +0100

    initial commit to code

---
+++ src/communication/ClientRunnable.java
@@ -0,0 +1,17 @@
+package communication;
+
+import java.net.Socket;
+
+public class ClientRunnable implements Runnable {
+
+	public ClientRunnable(Socket clientSocket) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/communication/Server.java
@@ -0,0 +1,42 @@
+package communication;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Server {
+
+	private static final int DEFAULT_PORT = 13337;
+	private ServerSocket ss;
+	
+	public Server(int port) throws IOException {
+		ss = new ServerSocket(port);
+	}
+	
+	public static void main(String[] args) {
+		int port = DEFAULT_PORT;
+		if (args.length >= 1) {
+			port = Integer.parseInt(args[0]);
+		}
+		
+		try {
+			Server server = new Server(port);
+			server.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("\n\nFailed to start server on port: " + port);
+		}
+		
+	}
+
+	private void start() {
+		while (true) {
+			try {
+				Thread t = new Thread(new ClientRunnable(ss.accept()));
+				t.start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
