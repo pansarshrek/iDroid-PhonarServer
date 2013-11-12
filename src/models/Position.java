da1ccf4e3c43e9904a6f52d53ce1370fab1ecb96
eriknylander <Erik>
2013-11-12 12:57:19 +0100

    Some stuff in Position

--- src/models/Position.java
+++ src/models/Position.java
@@ -6,4 +6,22 @@ public class Position {
 	private double longitude;
 	private double altitude;
 	
+	public Position(double latitude, double longitude, double altitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.altitude = altitude;
+	}
+	
+	public double getLatitude() {
+		return latitude;
+	}
+	
+	public double getLongitude() {
+		return longitude;
+	}
+	
+	public double getAltitude() {
+		return altitude;
+	}
+	
 }
