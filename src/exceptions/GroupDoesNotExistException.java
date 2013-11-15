eef1554d581bc8828f93457b9a823686cb20596b
eriknylander <Erik>
2013-11-15 10:36:42 +0100

    Removed ALL the group stuff

--- src/exceptions/GroupDoesNotExistException.java
+++
@@ -1,5 +0,0 @@
-package exceptions;
-
-public class GroupDoesNotExistException extends Exception {
-
-}
--- src/exceptions/GroupExistsException.java
+++
@@ -1,5 +0,0 @@
-package exceptions;
-
-public class GroupExistsException extends Exception {
-
-}
